53f1657942405bd13f39fbcb999e858ca92ea73e
Ronaldo jose <ronaldo.jrk@gmail.com>
2020-07-02 15:28:52 -0300

    adcionando nav bar e esqueleto do site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,128 @@
 import Head from 'next/head'
 
+
+
 export default function Home() {
   return (
+
+
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Decole.app</title>
+        <link rel="icon" href="/LogoCircle.svg" />
       </Head>
 
+
       <main>
+
+
         <nav className="navbar">
+          <div className="p-2 max-w-5xl m-auto flex justify-between items-center">
+            <img src="../decole_verdev2.svg" width={150} className="p-2 hidden md:inline-block" />
+            <img src="../LogoCircle.svg" width={60} className="p-2 md:hidden inline-block" />
+
+            <div className="p-2 flex items-center">
+              <a>Sobre</a>
+
+              <a>Baixar</a>
+            </div>
+          </div>
 
         </nav>
-        <session className="first_session" style={{backgroundColor: 'black'}}>
-          Teste
+        <session className="first_session">
+          <div className="p-2">
+
+            <h1 style={{ color: 'White', fontSize: 80 }}>Decole seu negócio no mundo digital! </h1>
+
+          </div>
+
         </session>
-        <session className="half_session" style={{backgroundColor: 'blue'}}>
+        <session className="half_session" >
           <div>
             <h1>Oi</h1>
+            <p>tesidoamsdpmasjdiasndmuiasjdmasuidajmsdpasdomsiodmsdokam,d,</p>
           </div>
         </session>
-        <session className="half_session" style={{backgroundColor: 'red'}}>
+        <session className="half_session" style={{ backgroundColor: 'red' }}>
           Teste
         </session>
-        <session className="fullscreen_session" style={{backgroundColor: 'yellow'}}>
-          Teste
+        <session className="faleconosco_session">
+
+
+
+          <div class="md:flex md:items-center">
+            <div class="md:w-1/3">
+
+            </div>
+            <br />
+            <br />
+            <div class="md:w-2/3">
+
+              <h1 classeName="p-2" style={{ color: '#009688', fontSize: 30 }}>Fale</h1>
+
+            </div>
+            <div class="md:w-3/3">
+
+              <h1 classeName="p-2" style={{ color: '#009688', fontSize: 30 }}>Conosco</h1>
+
+            </div>
+          </div>
+
+
+
+          <form class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-name">
+                  Nome
+                  </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" type="text" placeholder="Ex: Jane" />
+                <p class="text-red-500 text-xs italic">
+                  Por favor, preencha este campo.</p>
+              </div>
+
+
+              <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
+                  email
+                  </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-email" type="text" placeholder="Ex: suporte@decoleapp.com" />
+                <p class="text-red-500 text-xs italic">
+                  Por favor, preencha este campo.</p>
+              </div>
+            </div>
+
+
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-message">
+                  Messagem
+                  </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-message" type="text" placeholder="Ex: sua opinião" />
+                <p class="text-red-500 text-xs italic">
+                  Por favor, preencha este campo.</p>
+              </div>
+            </div>
+
+            <div class="md:flex md:items-center">
+              <div class="md:w-1/3"></div>
+              <div class=" btntext">
+                <button class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                  Enviar
+            </button>
+              </div>
+            </div>
+
+
+
+          </form>
+
         </session>
       </main>
 
-      <footer>
-        
+      <footer className="p-2 footer_session">
+        teste
       </footer>
-    </div>
+    </div >
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        green: '#009688',
+        green_primary: '#27DEBF',
+        green_dark: '#34494F',
+
+      },
+      textColor: {
+        primary: '#FFFFFF',
+        secondary: '#34494F',
+        dark: '#000000',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
