8997cdeb95838bae65f37f0d00d00d1ca26964e3
Ronaldo jose <ronaldo.jrk@gmail.com>
2020-07-04 12:46:36 -0300

    validação de formulario

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { FiInstagram, FiFacebook, FiMenu, FiMail } from 'react-icons/fi';
 import Rightnav from '../components/sidebar/Rightnav';
@@ -7,12 +7,62 @@ import Burger from '../components/sidebar/Burger';
 
 import Nav from '../components/nav';
 import Nav1 from '../components/sidebar/Navbar';
+import { Form } from '@unform/web';
+import Input from '../components/Form/input';
+import ReactSelect from '../components/Form/select';
+import * as Yup from 'yup';
+//simport swal from 'sweetalert';
+
 
 
 
 export default function Home() {
 
   var [open, setOpen] = useState(false)
+  const formRef = useRef(null);
+  const options = [
+    { value: 'sugestão', label: 'Sugestão' },
+    { value: 'dúvida', label: 'Dúvida' },
+    { value: 'reclamação', label: 'Reclamação' },
+    { value: 'outros', label: 'Outros' }
+  ]
+
+  async function handleSubmit(data, { reset }) {
+
+    //swal("Good job!", "You clicked the button!", "success");
+
+    try {
+
+      const screma = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string().email('Digite um email válido').required(' O email é obrigatório'),
+        message: Yup.string().required('A messagem é obrigatório'),
+        select: Yup.string().required('A opção é obrigatório'),
+      });
+      await screma.validate(data, { abortEarly: false, })
+      console.log(data);
+
+      formRef.current.setErrors({});
+
+      reset();
+    } catch (error) {
+      console.log(data);
+
+      if (error instanceof Yup.ValidationError) {
+
+        const errorMessages = {};
+
+        error.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        })
+
+        formRef.current.setErrors(errorMessages);
+
+      }
+
+    }
+
+  }
 
   return (
 
@@ -40,22 +90,20 @@ export default function Home() {
         </session>
         <session className="app_session" id="app">
           <div>
-            <h1>Oi</h1>
-            <p>tesidoamsdpmasjdiasndmuiasjdmasuidajmsdpasdomsiodmsdokam,d,</p>
+
+            <p className=" p-2 max-w-3xl m-1 flex justify-between text-secondary text-center text-justify text-sm sm:text-base md:text-xl lg:text-2xl xl:text-2xl">O Aplicativo foi desenvolvido para que seja rápido e fácil de aprender a integrar o seu negócio ao meio digital e trazendo a possibilidade de criar rede de parcerias e se impulsionar</p>
           </div>
         </session>
         <session className="somos_session" id="somos" >
 
-          <div className="p-2 max-w-3xl m-1 flex justify-between items">
+          <div className="p-2 max-w-3xl m-1 flex justify-between items-center">
 
-            <p className="text-white text-center text-justify text-sm sm:text-base md:text-xl lg:text-2xl xl:text-2xl">
-              Decole é um app especialista em conhecimento digital. ? sucesso online Criado em 2020, em
-              Salvador.
-              Nosso trabalho é proporcionar a melhor formação digital. Nosso principal diferencial é
-              proporcionar para microemp presença digital com ---- e ---- priorizando seu negócio.
-              Temos especialistas nos principais serviços Digitais: Google Ads, Facebook Ads, Instagram Ads,
-              Redes Sociais, E-mail Marketing, Hotsites, Criação e Otimização de Sites e SEO, e assim
-              prospectar os melhores contatos do mercado.
+
+
+            <p className="text-white text-center text-justify p-2 text-sm sm:text-base md:text-xl lg:text-2xl xl:text-2xl">
+              A Decole é uma empresa desenvolvedora de soluções no mundo digital, criada em 2020 com o intuito de fortalecer os negócios dos microempreendedores e assim prospectar os melhores contatos do mercado.
+              Nós da Decole, sabemos a importância da presença digital para o microempreendedor. Por isso nosso objetivo é proporcionar a melhor formação para você.
+
             </p>
 
           </div>
@@ -63,7 +111,7 @@ export default function Home() {
           <h2 className="text-white text-center text-justify text-xl sm:text-2xl"> Nosso Time</h2>
 
 
-          <div className="p-1 max-w-3xl  flex justify-between items-end">
+          <div className="p-1 max-w-3xl  flex justify-between items-center">
 
 
 
@@ -72,7 +120,7 @@ export default function Home() {
                 <div className="sm:flex sm:items-center">
                   <div className="sm:ml-4 sm:text-left text-center">
                     <p className="text-base md:text-xl lg:text-xl">Davi  Macedo  </p>
-                    <p className="text-sm text-gray-600">Programador</p>
+                    <p className="text-sm text-gray-600">Desenvolvedor</p>
 
                   </div>
                 </div>
@@ -84,7 +132,7 @@ export default function Home() {
                 <div className="sm:flex sm:items-center">
                   <div className="sm:ml-4 sm:text-left text-center">
                     <p className="text-base md:text-xl lg:text-xl">Eduardo Correia</p>
-                    <p className="text-sm text-gray-600">Programador</p>
+                    <p className="text-sm text-gray-600">Desenvolvedor</p>
 
                   </div>
                 </div>
@@ -97,7 +145,7 @@ export default function Home() {
                 <div className="sm:flex sm:items-center">
                   <div className="sm:ml-4 sm:text-left text-center">
                     <p className="text-base md:text-xl lg:text-xl">Guilherme Cunha</p>
-                    <p className="text-sm text-gray-600">Programador</p>
+                    <p className="text-sm text-gray-600">Desenvolvedor</p>
 
                   </div>
                 </div>
@@ -129,7 +177,7 @@ export default function Home() {
                 <div className="sm:flex sm:items-center">
                   <div className="sm:ml-4 sm:text-left text-center">
                     <p className="text-base md:text-xl lg:text-xl">João Carvalho</p>
-                    <p className="text-sm text-gray-600">Programador</p>
+                    <p className="text-sm text-gray-600">Desenvolvedor</p>
 
                   </div>
                 </div>
@@ -189,15 +237,14 @@ export default function Home() {
 
 
 
-          <form className="w-full max-w-lg">
+          <Form ref={formRef} className="w-full max-w-lg" onSubmit={handleSubmit}>
             <div className="flex flex-wrap ">
               <div className="w-full md:w-1/2 px-5 mb-6 md:mb-0">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-name">
                   Nome
                   </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" type="text" placeholder="Ex: Jane" />
-                <p className="text-red-500 text-xs italic">
-                  Por favor, preencha este campo.</p>
+                <Input name="name" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" type="text" placeholder="Ex: Jane" />
+
               </div>
 
 
@@ -205,9 +252,8 @@ export default function Home() {
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-email">
                   email
                   </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-email" type="text" placeholder="Ex: suporte@decoleapp.com" />
-                <p className="text-red-500 text-xs italic">
-                  Por favor, preencha este campo.</p>
+                <Input name="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-email" type="text" placeholder="Ex: suporte@decole.com" />
+
               </div>
             </div>
 
@@ -217,16 +263,30 @@ export default function Home() {
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-message">
                   Messagem
                   </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-message" type="text" placeholder="Ex: sua opinião" />
-                <p className="text-red-500 text-xs italic">
-                  Por favor, preencha este campo.</p>
+                <Input name="message" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-message" type="text" placeholder="Ex: sua opinião" />
+
               </div>
             </div>
+
+            <div className="flex flex-wrap ">
+              <div className="w-full px-5">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-message">
+                  Opções
+                  </label>
+                <ReactSelect defaultValue={{ value: "", label: "Escolha uma opção" }} options={options} name="select" className=" select block appearance-none w-full   px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+
+                </ReactSelect>
+
+              </div>
+            </div>
+
+
+
 
             <div className="md:flex md:items-center">
               <div className="md:w-1/3"></div>
               <div className="btntext">
-                <button className="  btntextpri shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded inline-block text-sm px-4 py-2 leading-none border rounded text-white hover:border-red hover:text-teal-500 hover:bg-white mt-4 lg:mt-0" type="button">
+                <button className="  btntextpri shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded inline-block text-sm px-4 py-2 leading-none border rounded text-white hover:border-red hover:text-teal-500 hover:bg-white mt-4 " type="submit">
                   Enviar
                 </button>
               </div>
@@ -234,7 +294,10 @@ export default function Home() {
 
 
 
-          </form>
+          </Form>
+
+
+
 
         </session>
       </main>
@@ -243,12 +306,18 @@ export default function Home() {
 
 
         <div className="h-screen max-w-5xl m-auto flex justify-between items-center">
-          <a>
+          <a href="https://www.facebook.com/Decole-104392271249723">
 
             <FiFacebook color={"#ffffffff"} className="rounded-full hover:bg-blue" />
           </a>
-          <FiInstagram color={"#ffffffff"} className="mx-10 rounded-full hover:bg-instagram" />
-          <FiMail color={"#ffffffff"} className="rounded-full hover:bg-red_primary" />
+          <a href="https://www.instagram.com/oficialdecole/?hl=pt-br">
+            <FiInstagram color={"#ffffffff"} className="mx-10 rounded-full hover:bg-instagram" />
+
+          </a>
+          {/*} <a>
+            <FiMail color={"#ffffffff"} className="rounded-full hover:bg-red_primary" />
+
+  </a>*/}
 
         </div>
       </footer>
